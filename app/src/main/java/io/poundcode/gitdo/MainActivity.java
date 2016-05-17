7aeee2a7e98bb196260dfcc019f1c3a25f3172be
Chris Pound <c2.pound@gmail.com>
2016-05-17 00:30:34 -0500

    Test data populating

--- app/src/main/java/io/poundcode/gitdo/MainActivity.java
+++ app/src/main/java/io/poundcode/gitdo/MainActivity.java
@@ -4,29 +4,49 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import io.poundcode.androidgithubapiwrapper.repository.GitHubRepository;
+import io.poundcode.gitdo.test.TestData;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+    @Bind(R.id.repositories)
+    RecyclerView repositories;
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if(fab != null)
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-            }
-        });
+        if (fab != null)
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                }
+            });
+        RecyclerView.LayoutManager manager = new GridLayoutManager(this, 2);
+        RepositoriesAdapter adapter = new RepositoriesAdapter();
+        repositories.setLayoutManager(manager);
+        repositories.setAdapter(adapter);
     }
 
     @Override
@@ -49,5 +69,47 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class RepositoriesAdapter extends RecyclerView.Adapter<RepositoryViewHolder> {
+        List<GitHubRepository> repositoryList = TestData.getGitHubRepositoryTestData();
+
+
+        @Override
+        public RepositoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = View.inflate(parent.getContext(), R.layout.item_repo, null);
+            return new RepositoryViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(RepositoryViewHolder holder, int position) {
+            GitHubRepository repository = repositoryList.get(position);
+            holder.tvRepoName.setText(repository.getName());
+            holder.tvRepoDescription.setText(repository.getDescription());
+            holder.tvOpenIssues.setText(String.valueOf(repository.getOpenIssuesCount()));
+        }
+
+        @Override
+        public int getItemCount() {
+            // TODO: 5/17/2016 replace with real data
+            return repositoryList.size();
+        }
+    }
+
+    public class RepositoryViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.tvRepoName)
+        TextView tvRepoName;
+        @Bind(R.id.tvRepoDescription)
+        TextView tvRepoDescription;
+        @Bind(R.id.tvOpenPullRequests)
+        TextView tvOpenPullRequests;
+        @Bind(R.id.tvOpenIssues)
+        TextView tvOpenIssues;
+
+        public RepositoryViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
     }
 }
